106e9afdf62857e057a6a9e56b7ff86f6d822251
Treeesk <yaroslav.kalinin.06@yandex.ru>
2026-04-03 22:30:13 +0300

    Fixed a bug that prevented error 500 from appearing.

--- backend/internal/handlers/handler.go
+++ backend/internal/handlers/handler.go
@@ -2,6 +2,7 @@ package handlers
 
 import (
 	"ProjectGo/backend/internal/services"
+	"bytes"
 	"encoding/json"
 	"log"
 	"net/http"
@@ -17,12 +18,14 @@ func NewHandlerNotes(store *services.NotesStore) *HandlerNotes {
 
 // Функция возвращающая JSON с полным списком всех заметок
 func (h *HandlerNotes) GetNotes(w http.ResponseWriter, r *http.Request) {
-	w.Header().Set("Content-Type", "application/json")
-	encod := json.NewEncoder(w)
-	err := encod.Encode(h.store.GetAll())
+	var buf bytes.Buffer
+	err := json.NewEncoder(&buf).Encode(h.store.GetAll())
 	if err != nil {
 		w.WriteHeader(http.StatusInternalServerError)
 		log.Println("Error: ", err)
 		return
 	}
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(http.StatusOK)
+	w.Write(buf.Bytes())
 }
