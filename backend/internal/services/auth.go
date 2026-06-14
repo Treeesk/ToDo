c7993e298f7a12802f4cbdaf9d57c2ae2c5fb92c
Treeesk <yaroslav.kalinin.06@yandex.ru>
2026-06-14 21:55:55 +0300

    WIP: add CreateJWT func

--- backend/internal/services/auth.go
+++ backend/internal/services/auth.go
@@ -1,6 +1,12 @@
 package services
 
-// Создание и проверка JWT токенов
+// Создание и проверка JWT токенов. Регистрация и логин пользователей.
+
+import (
+	"time"
+
+	"github.com/golang-jwt/jwt/v5"
+)
 
 type AuthService struct {
 	jwtSecret string
@@ -13,6 +19,21 @@ func NewAuthService(jwtSecret string) *AuthService {
 }
 
 // Функция по созданию JWT
-func (auth *AuthService) CreateToken() (string, error) {
+func (auth *AuthService) CreateToken(user_id int) (string, error) {
+	token := jwt.NewWithClaims(jwt.SigningMethodHS256,
+		jwt.MapClaims{
+			"sub": user_id,
+			"iat": jwt.NewNumericDate(time.Now()),
+			"exp": jwt.NewNumericDate(time.Now().Add(time.Minute * 15)),
+		})
+	tokenString, err := token.SignedString(auth.jwtSecret)
+	if err != nil {
+		return "", err
+	}
+	return tokenString, nil
+}
+
+// Функция проверки JWT
+func (auth *AuthService) VerifyToken(tokenString string) error {
 
 }
